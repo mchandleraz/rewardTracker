da571008006d5133ccc4466a64298c689089d1cf
Matt Chandler <mcaz689@gmail.com>
2017-01-29 12:12:22 -0700

    #yagni

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,17 +3,12 @@ const notImplemented = require('../../utils/notImplemented');
 function RootController() {};
 
 RootController.prototype.get = function(req, res, next) {
-  res.send(200);
+  res.send('Howdy.');
   next();
 }
 
-RootController.prototype.post = function(req, res, next) {
-  res.send(200, JSON.parse(req.body));
-  next();
-}
-
-RootController.prototype.put = notImplemented;
-
-RootController.prototype.del = notImplemented;
+RootController.prototype.post = notImplemented;
+RootController.prototype.put  = notImplemented;
+RootController.prototype.del  = notImplemented;
 
 module.exports = new RootController();
