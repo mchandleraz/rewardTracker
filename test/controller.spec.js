37b90b7740aca38c2281198d402f2dbc28d84d30
Matt Chandler <mcaz689@gmail.com>
2017-01-29 12:59:07 -0700

    add new test commands and fill out tests for rootController

--- test/controller.spec.js
+++ test/controller.spec.js
@@ -1,5 +1,41 @@
-import rootController from '../src/controllers';
+import { rootController } from '../src/controllers';
 
-test('root controller', function() {
-  expect(rootController.get(req, res, next)).toBe(true);
+let req;
+let res;
+let next;
+
+beforeEach(function() {
+  req = jest.fn();
+  res = {
+    send: jest.fn()
+  };
+  next = jest.fn();
+})
+
+test('rootController#get', function() {
+  rootController.get(req, res, next);
+
+  expect(res.send).toBeCalledWith('Howdy.');
+  expect(next).toBeCalled();
+});
+
+test('rootController#post', function() {
+  rootController.post(req, res, next);
+
+  expect(res.send).toBeCalledWith(501, {"error": "501 Not Implemented"});
+  expect(next).toBeCalled();
+});
+
+test('rootController#put', function() {
+  rootController.put(req, res, next);
+
+  expect(res.send).toBeCalledWith(501, {"error": "501 Not Implemented"});
+  expect(next).toBeCalled();
+});
+
+test('rootController#del', function() {
+  rootController.del(req, res, next);
+
+  expect(res.send).toBeCalledWith(501, {"error": "501 Not Implemented"});
+  expect(next).toBeCalled();
 });
